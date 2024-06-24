430717314add93970554f89162ab342393d8877e
minanadynarouz <mina.n.narouz@gmail.com>
2024-06-24 23:26:13 +0300

    Task 1

--- 0x00-Webpack/task_1/js/dashboard_main.js
+++ 0x00-Webpack/task_1/js/dashboard_main.js
@@ -1,18 +1,18 @@
 const $ = require("jquery");
+// Load the full build
 const _ = require("lodash");
 
+let count = 0;
 
-counter = 0
 function updateCounter() {
-  counter++;
-  $('#count').text(`${counter} clicks on the button`);
+  count++;
+  $("#count").text(`${count} clicks on the button`);
 }
 
 const $button = $("<button>Click here to get started</button>").on(
   "click",
   _.debounce(updateCounter, 500, { leading: true, trailing: false })
 );
-
 
 $('body').append("<p>Holberton Dashboard</p>");
 $('body').append("<p>Dashboard data for the students</p>");
