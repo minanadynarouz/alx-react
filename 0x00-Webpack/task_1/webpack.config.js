e597548d768a07a8133b36e1385985b8945c8c1a
minanadynarouz <mina.n.narouz@gmail.com>
2024-10-29 07:35:30 +0300

    Adding files for Task1

--- 0x00-Webpack/task_1/webpack.config.js
+++ 0x00-Webpack/task_1/webpack.config.js
@@ -1,10 +1,8 @@
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: 'production',
-  entry: './js/dashboard_main.js',  // Entry point for the application
-
+  entry: path.resolve(__dirname, './js/dashboard_main.js'),
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'public'),
