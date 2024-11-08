c1b88cdd4a933cc29f41d95566c0c98d02d05980
minanadynarouz <mina.n.narouz@gmail.com>
2024-11-08 07:54:02 +0200

    Task_4

---
+++ 0x03-React_component/task_4/dashboard/src/Footer/Footer.js
@@ -0,0 +1,13 @@
+import { getFullYear, getFooterCopy } from '../utils.js';
+import './Footer.css'
+
+export default function Footer() {
+  return (
+    <>
+      <hr className="footer_hr" />
+      <div className="App-footer" data-testid="footer">
+        <p>Copyright {getFullYear()} - {getFooterCopy(false)}</p>
+      </div>
+    </>
+  )
+}
